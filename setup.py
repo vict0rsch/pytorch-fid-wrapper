ee709ddbe383b86a29012b3ba0fa03168d2e7f10
vict0rsch <vsch@pm.me>
2020-11-01 10:42:46 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="pytorch-fid-wrapper",
+    version="0.0.1",
+    author="Victor Schmidt",
+    author_email="not.an.address@yes.com",
+    description=(
+        "Wrapper around the pytorch-fid package to compute Frechet Inception"
+        + "Distance (FID) using PyTorch in-memory given tensors of images."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vict0rsch/pytorch-fid-wrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires=">=3.5",
+    install_requires=["numpy", "pillow", "scipy", "torch", "torchvision"],
+)
